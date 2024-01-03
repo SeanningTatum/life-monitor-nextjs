0a95d59f8af36559323d0e944264d8fb52f0c431
Sean Urgel <developer@seanurgel.dev>
2024-01-03 12:08:32 -0500

    feat: create basic home page

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,11 +1,15 @@
+import { TypographyH2 } from "@/components/ui/typography";
 import { authOptions } from "@/lib/auth";
 import { redirectIfUnauthenticated } from "@/lib/utils"
-
 
 export default async function HomePage() {
   await redirectIfUnauthenticated(authOptions);
 
   return (
-    <h1>Home</h1>
+    <div className="align-self-end">
+      <div className="w-[300px]">
+        <TypographyH2>"Successful people are not gifted; they just work hard, then succeed on purpose." —G.K. Nielson</TypographyH2>
+      </div>
+    </div>
   )
 }
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function DashboardLayout({
   children
 }: PropsWithChildren): JSX.Element {
   return (
-    <>
+    <div className="min-h-screen flex flex-col h-full">
       <header className='sticky top-0 z-50 w-full border-b border-border/80 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
         <div className="flex h-16 items-center px-4">
           {/* <MainNav className="mx-6" /> */}
@@ -17,9 +17,10 @@ export default function DashboardLayout({
           </div>
         </div>
       </header>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <main className="container p-24 flex-1 flex">
         {children}
       </main>
-    </>
+    </div>
   )
 }
