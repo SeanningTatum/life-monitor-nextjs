3bc3f5dfb3766107882fe4a4526a1bb3339ced43
Sean Urgel <developer@seanurgel.dev>
2024-01-16 09:46:07 -0500

    feat: add more typography components

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -1,9 +1,37 @@
 import { type PropsWithChildren } from "react";
 
+export function TypographyH1({ children }: PropsWithChildren): JSX.Element {
+  return (
+    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      {children}
+    </h1>
+  );
+}
+
 export function TypographyH2({ children }: PropsWithChildren): JSX.Element {
   return (
     <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
       {children}
     </h2>
-  )
+  );
+}
+
+export function TypographyH3({ children }: PropsWithChildren): JSX.Element {
+  return (
+    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+      {children}
+    </h3>
+  );
+}
+
+export function TypographyH4({ children }: PropsWithChildren): JSX.Element {
+  return (
+    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+      {children}
+    </h4>
+  );
+}
+
+export function TypographyP({ children }: PropsWithChildren): JSX.Element {
+  return <p className="leading-7 [&:not(:first-child)]:mt-6">{children}</p>;
 }
