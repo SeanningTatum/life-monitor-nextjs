206ab45413c68325165b5d01a3cbba65d2f52ff3
Sean Urgel <developer@seanurgel.dev>
2024-01-16 11:47:06 -0500

    feat: optimistic updates on user-checklist

--- src/app/(authenticated)/dashboard/home/components/user-checklist.client.tsx
+++ src/app/(authenticated)/dashboard/home/components/user-checklist.client.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import dynamic from "next/dynamic";
 import { toast } from "sonner";
@@ -10,8 +10,8 @@ import useChecklist from "@/hooks/use-checklist";
 import { trpc } from "@/lib/trpc";
 import { Task, moveTask } from "@/lib/checklist";
 
-const Checklist = dynamic(() => import('@/components/checklist/index.client'), {
-  loading: () => <Skeleton className="h-[200px] w-full" />
+const Checklist = dynamic(() => import("@/components/checklist/index.client"), {
+  loading: () => <Skeleton className="h-[200px] w-full" />,
 });
 
 interface Props {
@@ -23,78 +23,115 @@ function UserChecklist(props: Props): JSX.Element {
     ...props.checklist,
     title: props.checklist.name,
     tasks: Object.fromEntries(
-      props.checklist.tasks.map(task => [task.id, { ...task }])
+      props.checklist.tasks.map((task) => [task.id, { ...task }])
     ),
   });
 
   const patchTask = trpc.checklist.patchTask.useMutation({
     onSuccess: (data) => {
       toast(`Updated ${data.title}!`);
-    }
+    },
   });
   const addTaskToChecklist = trpc.checklist.addTaskToChecklist.useMutation();
-  const deleteCompletedTasks = trpc.checklist.deleteCompletedTasks.useMutation({
-    onSuccess: () => {
-      toast('Removed completed tasks!');
-    }
-  });
+  const deleteCompletedTasks =
+    trpc.checklist.deleteCompletedTasks.useMutation();
   const deleteTaskById = trpc.checklist.deleteTaskById.useMutation();
   const patchChecklist = trpc.checklist.patchChecklist.useMutation();
 
   function completeTask(taskId: string): void {
-    patchTask.mutate({
-      id: taskId,
-      data: {
-        completed: !checklist.tasks[taskId].completed,
-      }
-    });
-
+    const snapshot = { ...checklist };
     actions.toggleTaskCompletion(taskId);
+
+    try {
+      patchTask.mutate({
+        id: taskId,
+        data: {
+          completed: !checklist.tasks[taskId].completed,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      setChecklist(snapshot);
+    }
   }
 
   function editTask(taskId: string, task: Partial<Task>): void {
-    patchTask.mutate({
-      id: taskId,
-      data: {
-        ...task,
-      }
-    })
-
+    const snapshot = { ...checklist };
     actions.updateTask(taskId, task);
+
+    try {
+      patchTask.mutate({
+        id: taskId,
+        data: {
+          ...task,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      setChecklist(snapshot);
+    }
   }
 
   async function onAddTask(title: string): Promise<void> {
     const mockId = Date.now().toString();
-    actions.addTask({ id: mockId, title, completed: false, optimistic: true, })
+    actions.addTask({ id: mockId, title, completed: false, optimistic: true });
+    const snapshot = { ...checklist };
 
-    const newTask = await addTaskToChecklist.mutateAsync({ checklistId: props.checklist.id, data: { title } })
-    actions.updateTask(mockId, { id: newTask.id, title, optimistic: false })
+    try {
+      const newTask = await addTaskToChecklist.mutateAsync({
+        checklistId: props.checklist.id,
+        data: { title },
+      });
+      actions.updateTask(mockId, { id: newTask.id, title, optimistic: false });
+    } catch (error) {
+      console.error(error);
+      setChecklist(snapshot);
+    }
   }
 
   async function onDeleteCompletedTasks(): Promise<void> {
-    await deleteCompletedTasks.mutateAsync({ checklistId: props.checklist.id });
+    const snapshot = { ...checklist };
     actions.deleteCompletedTasks();
+    try {
+      await deleteCompletedTasks.mutateAsync({
+        checklistId: props.checklist.id,
+      });
+    } catch (error) {
+      console.error(error);
+      setChecklist(snapshot);
+    }
   }
 
   async function onDeleteTask(taskId: string): Promise<void> {
-    await deleteTaskById.mutateAsync({ taskId, checklistId: props.checklist.id })
-    actions.deleteTask(taskId)
+    const snapshot = { ...checklist };
+    actions.deleteTask(taskId);
+
+    try {
+      await deleteTaskById.mutateAsync({
+        taskId,
+        checklistId: props.checklist.id,
+      });
+    } catch (error) {
+      console.error(error);
+      setChecklist(snapshot);
+    }
   }
 
   async function onTaskMoved(from: number, to: number): Promise<void> {
-    setChecklist(prev => {
+    setChecklist((prev) => {
       const newChecklist = moveTask(from, to, prev);
 
-      patchChecklist.mutate({
-        id: props.checklist.id,
-        data: {
-          taskOrder: newChecklist.taskOrder,
-        }
-      })
+      patchChecklist
+        .mutateAsync({
+          id: props.checklist.id,
+          data: {
+            taskOrder: newChecklist.taskOrder,
+          },
+        })
+        .catch(() => setChecklist(prev));
 
       return newChecklist;
-
-    })
+    });
   }
 
   return (
@@ -107,7 +144,7 @@ function UserChecklist(props: Props): JSX.Element {
       onEditTask={editTask}
       onTaskMoved={onTaskMoved}
     />
-  )
+  );
 }
 
-export default UserChecklist
+export default UserChecklist;
--- src/app/(authenticated)/dashboard/home/page.tsx
+++ src/app/(authenticated)/dashboard/home/page.tsx
@@ -1,6 +1,10 @@
 import UserChecklist from "./components/user-checklist.client";
 
-import { TypographyH2 } from "@/components/ui/typography";
+import {
+  TypographyH1,
+  TypographyLead,
+  TypographyP,
+} from "@/components/ui/typography";
 import { redirectIfUnauthenticated } from "@/lib/auth";
 import { serverRouter } from "@/server";
 
@@ -9,21 +13,34 @@ export default async function HomePage(): Promise<JSX.Element> {
 
   // Fetch first checklist
   // TODO:- Use the spread parameter to toggle between the checklists
-  let [checklist] = await serverRouter.checklist.getChecklistCollection({ userId: session.user.id }) ?? [];
+  let [checklist] =
+    (await serverRouter.checklist.getChecklistCollection({
+      userId: session.user.id,
+    })) ?? [];
 
   if (!checklist) {
-    checklist = await serverRouter.checklist.createChecklist({ userId: session.user.id });
+    checklist = await serverRouter.checklist.createChecklist({
+      userId: session.user.id,
+    });
   }
 
   return (
     <div className="flex justify-between w-full">
       <div className="w-[300px] self-end">
-        <TypographyH2>Hi</TypographyH2>
+        {session.user.name && (
+          <TypographyP>Good Morning, {session.user.name}</TypographyP>
+        )}
+        <TypographyH1 className="my-2">11:03PM</TypographyH1>
+        <TypographyLead>
+          “If you don’t see a clear path for what you want, sometimes you have
+          to make it yourself.”
+        </TypographyLead>
+        <TypographyP>Mindy Kaling</TypographyP>
       </div>
 
       <div className="w-[300px]">
         <UserChecklist checklist={checklist} />
       </div>
     </div>
-  )
+  );
 }
